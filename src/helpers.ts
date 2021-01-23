fa91dc5078a684495d977b38392468788cd0bb1d
Liam Read <liam@liam.pro>
2021-01-23 22:40:23 +0100

    :sparkles: init

---
+++ src/helpers.ts
@@ -0,0 +1,42 @@
+/**
+ * Format bytes as human-readable text.
+ *
+ * @param bytes Number of bytes.
+ * @param si True to use metric (SI) units, aka powers of 1000. False to use
+ *           binary (IEC), aka powers of 1024.
+ * @param dp Number of decimal places to display.
+ *
+ * @return Formatted string.
+ */
+const humanFileSize = (bytes: number, si = false, dp = 1): string => {
+  const thresh = si ? 1000 : 1024
+
+  if (Math.abs(bytes) < thresh) {
+    return `${bytes} B`
+  }
+
+  const units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB']
+  let u = -1
+  const r = 10 ** dp
+
+  do {
+    bytes /= thresh
+    ++u
+  } while (
+    Math.round(Math.abs(bytes) * r) / r >= thresh &&
+    u < units.length - 1
+  )
+
+  return `${bytes.toFixed(dp)} ${units[u]}`
+}
+
+/**
+ * Get the ref name from the ref string.
+ * @param {string} ref
+ * @return {string} The ref name.
+ */
+export const getRefName = (ref: string): string | null => {
+  return ref ? ref.split('/').slice(2).join('/') : null
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,114 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import {create, UploadOptions} from '@actions/artifact'
+import {context} from '@actions/github'
+import {getRefName} from './helpers'
+import {explore} from 'source-map-explorer'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    if (context.eventName) {
+      console.log(context.eventName)
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    return core.error('error')
 
-    core.setOutput('time', new Date().toTimeString())
+    // Fail for users trying to use this action outside of a pull request workflow..
+    if (!context.payload.pull_request) {
+      return core.error(
+        'This action is designed to be run against a pull request, make sure your workflow is valid..'
+      )
+    }
+
+    if (!process.env.GITHUB_REF) {
+      return core.error(
+        'The branch could not be detected, are we running in a CI?'
+      )
+    }
+
+    // Find the branch were currently on
+    const branch = getRefName(process.env.GITHUB_REF)
+
+    if (!branch) {
+      return core.error(
+        'The branch could name not be detected, does our branch name contain invalid chars?'
+      )
+    }
+
+    // Read in an input for the path in the case the bundle isn't collected directly in the root.
+    const path = core.getInput('path')
+
+    // No path has been specified, lets presume the build is located directly in the root.
+    if (!path) {
+      const outcomeBundle = await explore('./build/static/**/*.(js|css)', {
+        output: {format: 'json', filename: `${branch}-react-bundle-logs.json`}
+      })
+
+      if (outcomeBundle.bundles.length === 0) {
+        return core.error('The build output folder did not contain any files')
+      }
+    } else {
+      const outcomeBundle = await explore(
+        `${path}/build/static/**/*.(js|css)`,
+        {
+          output: {format: 'json', filename: `${branch}-react-bundle-logs.json`}
+        }
+      )
+
+      if (outcomeBundle.bundles.length === 0) {
+        return core.error('The build output folder did not contain any files')
+      }
+    }
+
+    // Create an artifact client to save current log
+    const artifactClient = create()
+
+    const options: UploadOptions = {
+      continueOnError: false
+    }
+
+    // Save a current log of what was built
+    const uploadResponse = await artifactClient.uploadArtifact(
+      branch,
+      [`./${branch}-react-bundle-logs.json`],
+      './',
+      options
+    )
+
+    if (uploadResponse) {
+      core.debug(
+        '⭐ A react bundle log for this build has been saved using your branch name!'
+      )
+    }
+
+    // Try and find a log to compare it too using the pull request destination
+    // get pull request target name:
+    const targetBranchName = getRefName(context.payload.pull_request.head.ref)
+
+    if (!targetBranchName) {
+      return core.error(
+        'The branch could name not be detected, does the target branch name contain invalid chars?'
+      )
+    }
+
+    try {
+      const foundArtifact = await artifactClient.downloadArtifact(
+        `./${targetBranchName}-react-bundle-logs.json`
+      )
+
+      if (!foundArtifact) {
+        // We couldn't find a corresponding branch name, this may mean that the target branch has never previously been built..
+        // At this point we can just set the output.
+
+        return core.debug(
+          '⭐ Set the bundle size without specifying what it was against!'
+        )
+      }
+    } catch (e) {
+      core.debug('Trying to find an artifact threw an error..')
+      return core.debug(
+        '⭐ Set the bundle size without specifying what it was against!'
+      )
+    }
   } catch (error) {
     core.setFailed(error.message)
   }
